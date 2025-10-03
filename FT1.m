b08d294a15ee94b438adfef106cf19ad294616ec
Hugo Diogo <hugodiogo@Hugos-MacBook-Air-8.local>
2025-10-03 17:57:01 +0100

    Updated FT1.m with corrections and improvements

--- FT1.m
+++ FT1.m
@@ -1,42 +1,57 @@
-f = 5.540;
-%f = input('Frequency': );
-
-
-%Output power
-P = @(x) 23 - 30 - (92.4 + 20 * log10(f) + 20 * log10(x / 1000));
+%choose the center frequency
+f = input('Frequency: ');
+%Choose the emission power (23dBm if it is WiFi 5G)
+emission = input('Emission power (dBm): ')
 
 
 
-N = input("Número de medições: ");
+%output power
+P = @(x) emission - 30 - (92.4 + 20 * log10(f) + 20 * log10(x / 1000));
+
+
+
+N = input("Number of measures: ");
 
 x = []; %distances vector
-y1 = []; %given values
+y1 = []; %the average of the given values
 
+%for each distance write five values
 for i = 1:N
     som = 0;
+    %here the values of the dstances ares going to be stored
     dist = input(sprintf('%d -> Distance in meters: ', i));
     x = [x, dist];
     for j = 1:5
+        %here the five values are going to be summed 
         som = som + input(sprintf('%d -> value nº%d: ', i, j));
     end
+    %here the algorithm will average the values and store the values
     som = (som / 5) - 30;
     y1 = [y1, som];
    
 end
 
+%here the function of the theoretical values will be created as y2
 y2 = P(x);
-erro = y2 - y1;
-erro2 = y1 - y2;
+
+%Empirical regression
+p = polyfit(x, y1, 5);   % the value 5 was choosen to best fit the points
+y1_fit = polyval(p, x);  % evaluates the polynomial function
+
+error = y1_fit - y2;
+
+
 figure(1)
-hold on
+hold on 
 xlim([x(1) x(N)])
-plot(x, y1, 'r')
-plot(x, y2, 'b')
-plot(x, erro, 'k')
+plot(x, y1, 'ro') %Empirical Data
+plot(x, y1_fit, 'r') %Empirical Regression
+plot(x, y2, 'b') %Theoretical
+plot(x, error, 'k') %Error
 xlabel('Distance (m)')
 ylabel('P(dB)')
-legend('Empirical', 'Theoretical', 'Error')
+legend('Empirical Data', 'Empirical Regression', 'Theoretical', 'Error', 'Location', 'best') %The last two inputs sets the discription at the best place
+
 saveas(gcf, 'graphic.png')
 
 
-# Fundamentals-in-Telecommunications-1st-Homework
